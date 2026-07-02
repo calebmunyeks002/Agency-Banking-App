bb64688b8fd7e8cb16f15518bb2cf7ab80946aee
Caleb <calebmunyeks002@gmail.com>
2026-07-02 14:27:12 +0300

    I have corrected the errors preventing running of the program in the auth.ts file

--- src/app/components/send/send.ts
+++ src/app/components/send/send.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-send',
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './send.html',
   styleUrl: './send.scss'
 })
 export class SendComponent {
   requirePinEntry = false;
   transactionPin = '';
-  sendData = { type: 'bank', myAccount: '', myPhone: '', recipient: '', amount: null };
+
+  sendData = {
+    type: 'bank',
+    myAccount: '',
+    myPhone: '',
+    recipient: '',
+    amount: null as number | null
+  };
 
   requestPinConfirmation() {
     this.requirePinEntry = true;
